40403af9c559a27ee56b0ee71c3c0cae12946c92
mirandov <tremors224@gmail.com>
2018-11-02 15:23:26 +0300

    master: master

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{log tmp/pids tmp/sockets public/system public/pdf}
+set :linked_dirs, %w{log tmp/pids tmp/sockets public/system public/assets public/pdf}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
