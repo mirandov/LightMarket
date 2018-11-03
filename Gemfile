eea278194ba5cbbecd9d7405638e14589f415250
mirandov <tremors224@gmail.com>
2018-11-04 02:27:53 +0300

    master: add image

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+gem 'lightbox2-rails'
 gem 'thin', '>= 1.6.0'
 # Use HAML for views
 gem 'haml' , '>= 4.0.0'
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -2,7 +2,7 @@ class Subcategory < ActiveRecord::Base
   belongs_to :category
   has_many   :products
   has_many   :shipments
-  has_attached_file :avatar, :styles => { thumb: ["300x300#", :png],
+  has_attached_file :avatar, :styles => { medium: ["180x200"], thumb: ["300x300#", :png],
                                                     original: [:png] }
   validates_attachment :avatar,
                      content_type: { content_type: /\Aimage\/.*\z/ },
