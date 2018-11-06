e2cc32acfe170c2af11f1000f564593c529857d3
mirandov <tremors224@gmail.com>
2018-11-06 16:04:13 +0300

    master: views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :subcategory
 
-  has_attached_file :avatar, :styles => { medium: ["100x100"], thumb: ["300x300#", :png],
+  has_attached_file :avatar, :styles => { medium: ["50x50"], thumb: ["300x300#", :png],
                                                     original: [:png] }
   validates_attachment :avatar,
                      content_type: { content_type: /\Aimage\/.*\z/ },
